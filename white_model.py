51984fdbdae953d679da7f5c16ad301448fdb598
Serena <serena.rem.lang@gmail.com>
2022-02-14 22:25:22 -0500

    White Wine model function

---
+++ white_model.py
@@ -0,0 +1,19 @@
+import pickle
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import StandardScaler
+import numpy as np
+
+def white_model(user_input_array):
+    #Load Model
+    with open('white_model.pkl', 'rb') as file:
+        white_model = pickle.load(file)
+    with open('scaler_white.pkl', 'rb') as file:
+        scaler = pickle.load(file)
+
+    X = user_input_array.reshape(1,-1)
+    X_scaled = scaler.transform(X)
+
+    # Calculate Wine quality
+    y_pred_white = white_model.predict(X_scaled)
+
+    return y_pred_white
